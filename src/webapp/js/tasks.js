063ef0fa718923244cfd636e9ea18cc173ce71d9
Katherine <182228@niuitmo.ru>
2015-12-13 12:46:14 +0300

    more js

--- src/webapp/js/tasks.js
+++ src/webapp/js/tasks.js
@@ -3,10 +3,11 @@
  */
 html = {
     task:
-    '<div class="task">'+
-    '   <i class="icon fa fa-square-o"></i>'+
-    '   <div contenteditable="true" class="task-descr"></div>'+
-    '</div>',
+        '<div class="task">'+
+        '   <i class="icon fa fa-square-o"></i>'+
+        '   <div contenteditable="true" class="task-descr"></div>' +
+        '   <i class="delete-icon fa fa-times-circle"></i>'+
+        '</div>',
 
     new_task:
         '<div class="task-item">'+
@@ -43,15 +44,22 @@ $(document).ready(function(){
 
     //Переключение активных тасков
     $(".task-container").on("click", ".task-item", function(){
-        $(this).addClass("active");
-        $(".task-item").not($(this)).removeClass("active");
+        if(!$(this).hasClass("active")){
+            //$(this).addClass("active");
+            if($(".task-item.active .tasks").html() == ""){
+                $(".task-item.active").remove();
+            } else{
+                $(".task-item").not($(this)).removeClass("active");
+            }
+            $(this).addClass("active");
 
-        taskDescr = $(".task-container .task-item.active .tasks-hidden .tasks").html();
-        $(".edit-window .tasks").html(taskDescr);
-        taskDone = $(".task-container .task-item.active .tasks-hidden .tasks-done").html();
-        $(".edit-window .tasks-done").html(taskDone);
-        taskTitle = $(this).find(".task-title").text();
-        $("#list-title").val(taskTitle);
+            taskDescr = $(".task-container .task-item.active .tasks-hidden .tasks").html();
+            $(".edit-window .tasks").html(taskDescr);
+            taskDone = $(".task-container .task-item.active .tasks-hidden .tasks-done").html();
+            $(".edit-window .tasks-done").html(taskDone);
+            taskTitle = $(this).find(".task-title").text();
+            $("#list-title").val(taskTitle);
+        }
     });
 
     //Заголовок задания
@@ -103,5 +111,25 @@ $(document).ready(function(){
     //Удаление активного таска
     $(".task-container").on("click", ".task-item.active .icon.task-remove", function(){
         $(".task-item.active").remove();
+    });
+
+    //Удаление задания из списка
+    $(".edit-container").on("click", ".delete-icon", function(){
+        $(this).parent().remove();
+
+        var taskDescr = $(".task-item.active .tasks-hidden .tasks");
+        taskDescr.html($(".edit-container .tasks").html());
+
+        //Первые три таска из task-hidden
+        all_tasks = $('.task-container .task-item.active .tasks-hidden .task:nth-child(-n+' + max_tasks + ')').clone();
+        all_tasks.find('div').attr('contenteditable', 'false');
+
+        tasks = $('.task-container .task-item.active > .tasks');
+        tasks.html(all_tasks);
+
+        //Если тасков больше, чем 3, то добавляются точечки после них
+        if($('.task-container .task-item.active .tasks-hidden .task:nth-child(4)').length != 0){
+            tasks.append('<i class="big-task fa fa-ellipsis-h"></i>');
+        }
     });
 });
