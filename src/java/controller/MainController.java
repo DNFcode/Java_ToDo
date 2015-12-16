2ee9103544e3d52b31c3a857b0b00fd490253aa6
Maxim Stepanov <maks1893@gmail.com>
2015-12-16 15:55:52 +0300

    ITS ALIVE v0.2!

--- src/java/controller/MainController.java
+++ src/java/controller/MainController.java
@@ -5,12 +5,19 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
 
+import javax.annotation.PostConstruct;
+
 @RestController
 public class MainController {
     //GET
     @RequestMapping(value = "/", method = RequestMethod.GET)
     public ModelAndView mainPage(){
         return new ModelAndView("hello");
+    }
+
+    @PostConstruct
+    public void OnStart(){
+        System.out.println("youAAAAA");
     }
 
 }
